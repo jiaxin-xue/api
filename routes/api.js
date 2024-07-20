eb2b050d50f395e2722ffa832946fb471daca474
jiaxin-xue <chloe12030908@gmail.com>
2024-07-20 12:23:37 -0400

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+	const sample = {
+		"browsers": {
+			"firefox": {
+				"name": "Firefox",
+				"pref_url": "about:config",
+				"releases": {
+					"1": {
+						"release_date": "2004-11-09",
+						"status": "retired",
+						"engine": "Gecko",
+						"engine_version": "1.7"
+					}
+				}
+			}
+		}
+	};
+    res.send(sample);
+});
+
+module.exports = router;
